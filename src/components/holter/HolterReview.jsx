87ac57e6300f21e08d663cdcf958576f7aa88f2a
Rimoosoo <studentlimsoo@gmail.com>
2023-09-17 17:35:00 +0900

    chore : holter test 수정

---
+++ src/components/holter/HolterReview.jsx
@@ -0,0 +1,7 @@
+import { ViewContainer } from "./HolterReview.style";
+
+function HolterReview() {
+  return <ViewContainer></ViewContainer>;
+}
+
+export default HolterReview;
---
+++ src/components/holter/HolterReview.style.js
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+import { FlexBox } from "../../styles/";
+
+export const ViewContainer = styled(FlexBox);
